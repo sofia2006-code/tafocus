8093ede34ac979d5136b5d47409c954d0a8c5d21
AmbarJaichenco <ambarjazminjaichenco@gmail.com>
2023-05-30 10:37:18 -0300

    Fix: set up

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export default function index() {
+	return (
+		<div className="bg-gray-900">
+            <div className="max-w-2xl min-h-screen bg-gray-100">
+                <h1>Hello World</h1>
+            </div>
+        </div>
+	);
+}
